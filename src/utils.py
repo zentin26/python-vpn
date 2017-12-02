1c3c4be633d3aabc1b603142bdb9fbda93b7d6a3
zentin26 <hunterlip46@gmail.com>
2017-12-02 14:40:37 +1100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import yaml
 
 
 def index_tuples(l, v, c=0):
+    # Return the first value in an array of tuples in which the value in the specific column matches v
     return filter(lambda x: x[c] == v, l)[0]
 
 def load_config():
