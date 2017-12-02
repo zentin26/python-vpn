5ee9b82c5b7f348dff9032e493631e2efe060dae
zentin26 <hunterlip46@gmail.com>
2017-12-02 14:43:23 +1100

    Update frame.py

--- src/frame.py
+++ src/frame.py
@@ -2,6 +2,7 @@ from twisted.internet.task import LoopingCall
 from twisted.internet import reactor, protocol
 
 class FrameReciver(protocol.DatagramProtocol):
+    """Recieves incoming datagrams and passes them to the server the specificied frame class"""
     def __init__(self, server, frame_class):
         self.server = server
         self.frame_class = frame_class
@@ -12,13 +13,16 @@ class FrameReciver(protocol.DatagramProtocol):
         
         
 class Frame:
+    """Base frame class"""
     def __init__(self, transport, data, host):
-        self.transport = transport
-        self.host = host
+        self.transport = transport # the data channel
+        self.host = host # the sending host
         self.parse_data(data)
         
     def parse_data(self, data):
+        # to be over-written
         self.data = data
         
     def reply(self, data):
+        # to be over-written
         self.transport.write(data, self.host)
